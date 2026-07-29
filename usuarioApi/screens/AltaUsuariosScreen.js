a87229787cd25bac60b1cf9cd1bfc7c2e1da59c5
mhaHub <123046406@upq.edu.mx>
2026-07-28 22:57:52 -0600

    Práctica No.20 Conexión Móvil API: Select & Insert

--- usuarioApi/screens/AltaUsuariosScreen.js
+++ usuarioApi/screens/AltaUsuariosScreen.js
@@ -1,9 +1,53 @@
 import React, { useState } from 'react';
-import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,} from 'react-native';
+import {View,SafeAreaView,Text,TextInput,Pressable,StyleSheet,Alert,Platform} from 'react-native';
 
 export default function App() {
   const [nombre, setNombre] = useState('');
   const [edad, setEdad] = useState('');
+  const [cargando, setCargando] = useState(false);
+
+  const mostrarMensaje = (titulo,mensaje) => {
+    if(Platform.OS === 'web'){
+      window.alert(`${titulo}\n${mensaje}`);
+    }else{
+      Alert.alert(titulo,mensaje)
+    }
+  };
+
+  const guardarUsuario = async() => {
+    if(nombre.trim() === '' || edad.trim() === ''){
+      mostrarMensaje("Vacios", "Campos Obligatorios ")
+      return;
+    }
+
+    try{
+      setCargando(true)
+      const respuesta = await fetch('http://192.168.100.95:5000/v1/usuarios',
+        {
+          method:"POST",
+          headers:{"Content-type": "application/json"},
+          body: JSON.stringify({nombre:nombre, edad:Number(edad)})
+        }
+
+      );
+      const datos = await respuesta.json();
+
+      console.log("Respuesta API: ",datos);
+      mostrarMensaje("Exito", "Usuario Registrado");
+
+      setNombre('');
+      setEdad('');
+
+
+    }catch(error){
+      console.log("Error API", error);
+      mostrarMensaje("Error", "No fue posible guardar");
+    }
+    finally{
+      setCargando(false);
+    }
+  }
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,9 +73,9 @@ export default function App() {
           onChangeText={setEdad}
         />
 
-        <Pressable style={styles.boton}>
+        <Pressable style={styles.boton} onPress={guardarUsuario} disabled = {cargando}>
           <Text style={styles.textoBoton}>
-            Agregar Usuario
+            {cargando? "Guardando...":"Agregar Usuario"}
           </Text>
         </Pressable>
 
--- usuarioApi/screens/ConsultaUsuariosScreen.js
+++ usuarioApi/screens/ConsultaUsuariosScreen.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import {SafeAreaView,View,Text,FlatList,StyleSheet,
-} from 'react-native';
+import React,{useState, useEffect} from 'react';
+import {SafeAreaView,View,Text,FlatList,StyleSheet,} from 'react-native';
 
 export default function ConsultaUsuariosScreen() {
 
-  const usuarios = [
-    { id: '1', nombre: 'Isay Guerra', edad: 22 },
-    { id: '2', nombre: 'Ana López', edad: 19 },
-    { id: '3', nombre: 'Carlos Gonzalez', edad: 25 },
-    { id: '4', nombre: 'Bjork Guerra', edad: 21 },
-    { id: '5', nombre: 'Luisa Martínez', edad: 28 },
-  ];
+  const [usuarios, setUsuarios] = useState([]);
+
+  const obtenerUsuarios = async ()=>{
+    try{
+      const respuesta = await fetch('http://192.168.100.95:5000/v1/usuarios');
+      const datos = await respuesta.json();
+      console.log("Respuesta API: ", datos);
+      setUsuarios(datos.usuarios)
+    }catch(error){
+      console.log("Error API: ", error);
+    }
+  };
+
+  useEffect(() => {obtenerUsuarios();},[])
 
   const renderTarjeta = ({ item }) => (
     <View style={styles.card}>
